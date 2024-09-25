465c2f7649e85ea1feb74cc3f3bf81b9208b6ecf
Xiaojing Wang <2002434+wangxj03@users.noreply.github.com>
2024-09-24 22:51:10 -0700

    Reorganize ingestion code

--- code-search/ingestion/run.sh
+++ code-search/ingestion/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python jobs/code_split.py --input_dir=/qdrant --output_file=/data/code_chunks.parquet
-python jobs/code_embed.py --input_file=/data/code_chunks.parquet --output_file=/data/code_embeddings.parquet
-python jobs/code_index.py --qdrant_host=http://qdrant:6333 --input_file=/data/code_embeddings.parquet
-python jobs/file_index.py --qdrant_host=http://qdrant:6333 --input_dir=/qdrant
+python src/code_split.py --input_dir=/qdrant --output_file=/data/code_chunks.parquet
+python src/code_embed.py --input_file=/data/code_chunks.parquet --output_file=/data/code_embeddings.parquet
+python src/code_index.py --qdrant_host=http://qdrant:6333 --input_file=/data/code_embeddings.parquet
+python src/file_index.py --qdrant_host=http://qdrant:6333 --input_dir=/qdrant
